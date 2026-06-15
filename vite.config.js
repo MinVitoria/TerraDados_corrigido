0901907865aefbaba81ea58e2b0916a161dbf220
MinVitoria <yasminvitoriac18@gmail.com>
2026-06-14 21:18:31 -0800

    Corrige deploy GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
-import base44 from "@base44/vite-plugin"
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
-  // IMPORTANTE: Mude o nome abaixo para o nome exato do seu repositório
-  base: '/TerraDados_corrigido/',   
-
-  logLevel: 'error',
-  plugins: [
-    base44({
-      legacySDKImports: true,
-      hmrNotifier: true,
-      navigationNotifier: true,
-      analyticsTracker: true,
-      visualEditAgent: true
-    }),
-    react(),
-  ]
+  plugins: [react()],
+  base: "/TerraDados_corrigido/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
